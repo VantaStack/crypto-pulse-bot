e18dd684ea57af068ed9720309984a2ada471880
VantaStack <amiralimahmoudina@gmail.com>
2025-09-27 22:50:06 +0330

    Update logger.js

--- config/utils/logger.js
+++ config/utils/logger.js
@@ -1,16 +1,9 @@
-// utils/logger.js
-const winston = require("winston");
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `[${timestamp}] ${level}: ${message}`;
-    })
-  ),
-  transports: [new winston.transports.Console()],
+const pino = require('pino');
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: { colorize: true }
+  }
 });
-
 module.exports = logger;
