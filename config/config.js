5c5ac50b263af4e08b6e8bfa52b6c948513137dd
VantaStack <amiralimahmoudina@gmail.com>
2025-09-29 15:45:52 +0330

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { TELEGRAM_TOKEN, PREFERRED_PROVIDERS, NODE_ENV } = process.env;
-
-module.exports = {
-  TELEGRAM_TOKEN,
-  PREFERRED_PROVIDERS: PREFERRED_PROVIDERS ? PREFERRED_PROVIDERS.split(',').map(s => s.trim()) : ['coingecko','binance'],
-  NODE_ENV: NODE_ENV || 'development',
-};
--- config/utils/utils/formatter.js
+++
@@ -1,7 +0,0 @@
-function formatPrice(num) {
-  if (num === null || num === undefined || Number.isNaN(num)) return 'N/A';
-  if (num >= 1) return Number(num).toLocaleString(undefined, { maximumFractionDigits: 6 });
-  return Number(num).toPrecision(6);
-}
-
-module.exports = { formatPrice };
